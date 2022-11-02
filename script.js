6fb3d02ecb6744dc905e5fe9af0741d1cfe43698
future-leader1 <mr.alijonov.azizbek@gmail.com>
2022-11-02 18:43:38 +0900

    new Feature Implementing Observer

--- script.js
+++ script.js
@@ -134,17 +134,50 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 ///stiicky event
 
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-console.log(initialCoords);
-window.addEventListener('scroll', function (e) {
-  console.log(window.scrollY);
-  if (this.window.scrollY > initialCoords.top) {
-    nav.classList.add('sticky');
-  } else {
-    nav.classList.remove('sticky');
-  }
+// console.log(initialCoords);
+// window.addEventListener('scroll', function (e) {
+//   if (this.window.scrollY > initialCoords.top) {
+//     nav.classList.add('sticky');
+//   } else {
+//     nav.classList.remove('sticky');
+//   }
+// });
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// };
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+
+// observer.observe(section1);
+
+const header = document.querySelector('header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stikyNav = function (entries) {
+  const entry = entries[0];
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stikyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `${navHeight}px`,
 });
+
+headerObserver.observe(header);
 
 // console.log(document.documentElement);
 // console.log(document.head);
